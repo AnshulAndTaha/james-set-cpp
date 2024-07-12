8314ea5fbcf7d9f6c9015ffb791f669a133bed3f
Taha Nil <taha.nilfo@gmail.com>
2024-07-11 22:37:50 -0500

    Tetration Implemented

--- Main.cpp
+++ Main.cpp
@@ -1,113 +1,16 @@
 #include <iostream>
 #include <cmath>
-#include <set>
-#include <vector>
-
-using std::set;
-using std::vector;
-
-int tetrate(int base, int height);
-
-vector<int>& foreignOperations(const set<int>& population);
-vector<int>& domesticOperations(const set<int>& population);
-
-void updateSet(const vector<int>& foreignPopulation, const vector<int>& domesticPopulation, vector<set<int>> population);
-void updateAlien(const vector<set<int>>& populations, const vector<int>& alienNumbers){};
-
-void runCycle(vector<set<int>>* populations, vector<int> alienNumbers, int cycleCount){};
-void printResult(const vector<set<int>>& populations, const vector<int>& alienNumbers){};
-
-void insert(vector<int>& values, int value) {
-    if(shouldSort) {
-        for(int i = 0; i < values.size(); i++) {
-            if(value < values[i]) {
-                values.insert(values.begin() + i, value);
-                return;
-            }
-        }
-    }
-    values.push_back(value);
-}
-
-// ADJUSTABLE PARAMETERS
-int tetrationLimit = 1;
-bool shouldSort = false;
-
-int main() {
-    vector<set<int>> populations = {};
-    vector<int> alienNumbers = {};
-    
-    runCycle(&populations, alienNumbers, 1);
-    
-    printResult(populations, alienNumbers);
-
-    return 0;
-}
-
-
-void runCycle(vector<set<int>>* populations, vector<int> alienNumbers, int cycleCount) {
-    vector<int> sortedVals;
-    for(int i = 0; i < cycleCount; i++) {
-        updateSet(
-            foreignOperations((*populations)[i]), 
-            domesticOperations((*populations)[i]), 
-            populations);
-
-        
-    }
-}
-
-void printResult(const vector<set<int>>& populations, const vector<int>& alienNumbers){
-    int index = 1;
-    for(set<int> population : populations) {
-        for(int num : population) {
-            printf("Population %d: %d\n", index, num);
-            printf("Alien %d: %d\n", index, alienNumbers[index - 1]);
-            printf("\n");
-        }
-        index++;
-    }
-
-}
-
-void updateSet(const vector<int>& foreignPopulation, const vector<int>& domesticPopulation, vector<set<int>>* populations){
-    set<int> newPopulation;
-
-    for(int num : foreignPopulation) newPopulation.insert(num);
-    for(int num : domesticPopulation) newPopulation.insert(num);
-
-    (*populations).push_back(newPopulation);
-}
-
-vector<int>& foreignOperations(const set<int>& population) {
-    vector<int> popVals(population.begin(), population.end());
-    vector<int> values = {};
-    for(int i = 0; i < population.size(); i++) {
-        for(int j = i + 1; j < population.size(); j++) {
-            values.push_back(popVals[i] + popVals[j]);
-            values.push_back(popVals[i] * popVals[j]);
-            values.push_back(std::pow(popVals[i], popVals[j]));
-            values.push_back(std::pow(popVals[j], popVals[i]));
-            if(j > tetrationLimit) values.push_back(tetrate(i, j));
-            if(i > tetrationLimit) values.push_back(tetrate(j, i));
-        }
-    }
-    return values;
-};
-
-vector<int>& domesticOperations(const set<int>& population) {
-    vector<int> values {};
-    for(int num : population) {
-        values.push_back(num + num);
-        values.push_back(num * num);
-        values.push_back(std::pow(num, num));
-        if(num > tetrationLimit) values.push_back(tetrate(num, num));
-    }
-    return values;
-};
 
 int tetrate(int base, int height) {
+    if(height < 0) return -1;
+    if(height == 0) return 1;
+
     int result = base;
-    for (int i = 1; i < height; i++) result = pow(base, result);
+    for(int i = 1; i < height; i++) result = std::pow(result, base);
     return result;
+}
+
+int main() {
+    std::cout << tetrate(3, 2) << std::endl;
+    return 0;
 }
