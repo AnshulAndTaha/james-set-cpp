a03727a1b05d8c147ec4918853d50d500e8f7c35
Taha Nil <taha.nilfo@gmail.com>
2024-07-11 23:15:23 -0500

    Created foreign and domestic operations

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+
+using std::vector;
+
+// GLOBAL VARIABLES
+vector<vector<int>> populations {};
+vector<int> alienNumbers {};
 
 int tetrate(int base, int height) {
     if(height < 0) return -1;
@@ -10,7 +17,44 @@ int tetrate(int base, int height) {
     return result;
 }
 
+
+vector<int> domesticOperations(vector<int> population) {
+    vector<int> domesticOps = {};
+    for(int num : population) {
+        domesticOps.push_back(num + num);
+        domesticOps.push_back(num * num);
+        domesticOps.push_back(std::pow(num, num));
+        domesticOps.push_back(tetrate(num, num));
+    }
+    return domesticOps;
+}
+
+vector<int> foreignOperations(vector<int> population) {
+    vector<int> foreignOps = {};
+    for(int i = 0; i < population.size(); i++) {
+        for(int j = i + 1; j < population.size(); j++) {
+            foreignOps.push_back(population[i] + population[j]);
+            foreignOps.push_back(population[i] * population[j]);
+            foreignOps.push_back(std::pow(population[i], population[j]));
+            foreignOps.push_back(std::pow(population[j], population[i]));
+            foreignOps.push_back(tetrate(population[i], population[j]));
+            foreignOps.push_back(tetrate(population[j], population[i]));
+        }
+    }
+    return foreignOps;
+}
+
+void printCycle(int idx) {
+    std::cout << "\nCycle #" << idx << "\nPopulation: ";
+    for(int num : populations[idx - 1]) std::cout << num << ", ";
+    std::cout << "\nAlien: " << alienNumbers[idx - 1] << "\n";
+}
+
+void printResult(){
+    for(int i = 1; i <= populations.size(); i++) printCycle(i);
+}
+
 int main() {
-    std::cout << tetrate(3, 2) << std::endl;
+    printResult();
     return 0;
 }
